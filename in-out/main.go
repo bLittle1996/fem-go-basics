910afb85a03836c6121211eebfab63a2501e610a
Ben Littleton <b.little1996@gmail.com>
2023-10-21 12:53:46 -0400

    finished section

--- in-out/main.go
+++ in-out/main.go
@@ -9,8 +9,14 @@ func init() {
 	fmt.Println("Hello from main.go")
 }
 
+func doSomeMaths(number float64) (float64, float64) {
+	return number * 4.20, number * 0.69
+}
+
 func main() {
 	data.SomeDict["haha"] = data.FunnyNumber == 42069
 	fmt.Printf("%v\n", data.SomeDict)
 	printSomethingIdk()
+	a, b := doSomeMaths(40.0)
+	fmt.Printf("%f, %f\n", a, b)
 }
